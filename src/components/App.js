7e36cccfe6a2a361a7b1d00ae7cf87ae2de65a01
VladimirJarabica <vladojarabica@gmail.com>
2017-03-01 17:46:07 +0100

    message list start

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react"
-import { connect } from "react-redux"
+
+import MessagesList from "./messages/MessagesList"
+import Input from "./input/Input"
 
 class App extends Component {
 	render() {
 		return (
-			<div>App</div>
+			<div>
+				<div>
+					<MessagesList/>
+				</div>
+				<div>
+					<Input/>
+				</div>
+			</div>
 		)
 	}
 }
 
-export default connect()(App)
+export default App
---
+++ src/components/messages/MessagesList.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react"
+
+class MessagesList extends Component {
+	render() {
+		return (
+			<div>Message list</div>
+		)
+	}
+}
+
+export default MessagesList
