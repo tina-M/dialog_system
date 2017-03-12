c263622cc2fb8d38cb5d58194682a6222918b443
VladimirJarabica <vladojarabica@gmail.com>
2017-03-12 09:44:50 +0100

    add package.json, simple server

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express')
+var path = require('path')
+var app = express()
+
+app.use(express.static('public'))
+
+// Path to render index html
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname, './demo', 'index.html'))
+})
+
+// Path to return answer for message (will be replaced by api)
+app.get('/message', function (req, res) {
+	res.send('Answer xyz')
+})
+
+app.listen(3000, function () {
+	console.log('App listening on port 3000')
+})
